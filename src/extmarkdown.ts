9aa428f86b33b961e68976aa88af6f96a2e16009
metaleap <metaleap@mailbox.org>
2017-03-16 20:45:40 +0100

    [autocommit] «Is there life before breakfast?»

--- src/extmarkdown.ts
+++ src/extmarkdown.ts
@@ -5,15 +5,15 @@ import vscmd = vs.commands
 import vslang = vs.languages
 import vswin = vs.window
 
-const _md = 'markdown'
-const codeAction_ToUpper = { arguments: [ true ], command: 'expo.mdUpOrLo', title: "toUpper: invokes `expo.mdUpOrLo` command to call `onActionToUpOrLo()`" }
-const codeAction_ToLower = { arguments: [ false ], command: 'expo.mdUpOrLo', title: "toLower: invokes `expo.mdUpOrLo` command to call `onActionToUpOrLo()`" }
+const _md               = 'markdown'
+const codeAction_Replace = { arguments: [], command: 'expo.replaceExpo',
+                            title: "Replace with 'EXPO' (invokes `expo.replaceExpo` command, which invokes `onCodeAction_Replace()`)" }
 
 
 export function onActivate (vsctx :vs.ExtensionContext, d :vs.Disposable[]) {
-    d.push(vscmd.registerTextEditorCommand('expo.mdUpOrLo', onActionToUpOrLo))
-    d.push(vslang.registerHoverProvider(_md, { provideHover: onHover }))
-    d.push(vslang.registerCodeActionsProvider(_md, { provideCodeActions: onActions }))
+    d.push( vscmd.registerTextEditorCommand('expo.replaceExpo', onCodeAction_Replace) )
+    d.push( vslang.registerHoverProvider(_md, { provideHover: onHover }) )
+    d.push( vslang.registerCodeActionsProvider(_md, { provideCodeActions: onActions }) )
 }
 
 
@@ -23,21 +23,13 @@ function onHover (doc :vs.TextDocument, pos :vs.Position, cancel :vs.Cancellatio
 }
 
 function onActions (doc :vs.TextDocument, range :vs.Range, ctx :vs.CodeActionContext, cancel :vs.CancellationToken) {
-    const actions :vs.Command[] = []
-    const ed = vswin.activeTextEditor
-    // const doctxt = doc.getText(range) // either selection OR current word under text-cursor
-    const edtxt = ed ? ed.document.getText(ed.selection) : null
-    if (edtxt) {
-        console.log("["+edtxt+"]")
-        actions.push(codeAction_ToUpper)
-        actions.push(codeAction_ToLower)
-    }
+    const actions :vs.Command[] = [ codeAction_Replace ]
+    // const doctxt = doc.getText(range) // this is either selection-text OR current-word-under-caret
     return actions
 }
 
-function onActionToUpOrLo (ed :vs.TextEditor, op :vs.TextEditorEdit, upper :boolean) {
-    const txt = ed.document.getText(ed.selection)
-    if (txt)
-        for (const sel of ed.selections)
-            op.replace(sel, upper ? txt.toUpperCase() : txt.toLowerCase())
+function onCodeAction_Replace (ed :vs.TextEditor, op :vs.TextEditorEdit, ...args :any[]) {
+    for (const sel of ed.selections) {
+        op.replace(sel, "EXPO")
+    }
 }
