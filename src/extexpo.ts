7be9e25872ac792fc6f7642024d9df9d0db0bd61
metaleap <metaleap@mailbox.org>
2017-03-16 14:41:36 +0100

    [autocommit] «waste water tank overflowed onto computer»

--- src/extexpo.ts
+++ src/extexpo.ts
@@ -6,6 +6,9 @@ import vswin = vscode.window
 import vsprj = vscode.workspace
 
 
+import { ChildProcess } from 'child_process';
+
+
 let vsOut   :vscode.OutputChannel
 let nterms  :number                 = 0
 const lot   :vscode.Disposable[]    = []
@@ -36,15 +39,16 @@ export function activate (context :vscode.ExtensionContext) {
 
     vsprj.onDidChangeConfiguration(onCfgChanged)
     vsprj.onDidChangeTextDocument(onDocChanged)
-    vsprj.onDidCloseTextDocument( (doc) => onDocEvent(doc, "closed") )
-    vsprj.onDidOpenTextDocument( (doc) => onDocEvent(doc, "opened from") )
-    vsprj.onDidSaveTextDocument( (doc) => onDocEvent(doc, "saved to") )
-    vswin.onDidChangeActiveTextEditor((_) => putStrLn("window.onDidChangeActiveTextEditor"))
-    vswin.onDidChangeTextEditorOptions((_) => putStrLn("window.onDidChangeTextEditorOptions"))
-    vswin.onDidChangeTextEditorSelection((sel) => { if (sel && sel.kind) putStrLn("window.onDidChangeTextEditorSelection") })
-    vswin.onDidChangeTextEditorViewColumn((_) => putStrLn("window.onDidChangeTextEditorViewColumn"))
-    vswin.onDidChangeVisibleTextEditors((_) => putStrLn("window.onDidChangeVisibleTextEditors"))
-    vswin.onDidCloseTerminal((_) => putStrLn("window.onDidCloseTerminal"))
+    vsprj.onDidCloseTextDocument( (doc)=> onDocEvent(doc, "closed") )
+    vsprj.onDidOpenTextDocument( (doc)=> onDocEvent(doc, "opened from") )
+    vsprj.onDidSaveTextDocument( (doc)=> onDocEvent(doc, "saved to") )
+    vswin.onDidChangeActiveTextEditor( (_)=> putStrLn("window.onDidChangeActiveTextEditor") )
+    vswin.onDidChangeTextEditorOptions( (_)=> putStrLn("window.onDidChangeTextEditorOptions") )
+    vswin.onDidChangeTextEditorSelection( (sel)=> { if (sel && sel.kind) putStrLn("window.onDidChangeTextEditorSelection") } )
+    vswin.onDidChangeTextEditorViewColumn( (_)=> putStrLn("window.onDidChangeTextEditorViewColumn") )
+    vswin.onDidChangeVisibleTextEditors( (_)=> putStrLn("window.onDidChangeVisibleTextEditors") )
+    vswin.onDidCloseTerminal( (_)=> putStrLn("window.onDidCloseTerminal") )
+    let vsl :vscode.LanguageConfiguration
 
     const txtgen = { provideTextDocumentContent : demoTextGen }
     addDisp(vsprj.registerTextDocumentContentProvider('expo', txtgen))
@@ -52,23 +56,26 @@ export function activate (context :vscode.ExtensionContext) {
 
 
 
-function handleError (err :any) {
+function onReject (err :any) {
+    // used for almost all promises in *this* script, *but* in practice IMHO needless clutter for
+    // "robust/quasi-unbreakable"/`undefined`-returning-on-cancellation APIs like showErrorMessage etc
     vswin.showErrorMessage("Error/cancellation/issue: "+err)
 }
 
 function demoMsgInfo () {
     vswin.showInformationMessage("demoMsgInfo ➜ this is `vscode.window.showInformationMessage` in action")
-        .then((_) => vswin.showInputBox().then(putStrLn))
+        .then( (_)=> vswin.showInputBox().then(putStrLn) )
 }
 
 function demoMsgErr () {
     vswin.showErrorMessage("demoMsgErr ➜ this is `vscode.window.showErrorMessage` in action", "QuickPick palette..")
-        .then( (pal) => { if (pal) vswin.showQuickPick(["A choice here..","another there..","pick your choice son!"]).then((pick) => putStrLn(pick)) } , handleError )
+        .then( (palclicked)=> { if (palclicked) vswin.showQuickPick(["A choice here..","another there..","pick your choice son!"])
+            .then(putStrLn , onReject) } , onReject )
 }
 
 function demoMsgWarn () {
     vswin.showWarningMessage("demoMsgWarn ➜ this is `vscode.window.showWarningMessage` in action", "Do dis", "Do dat")
-        .then((choice) => putStrLn(choice ? choice : "(dismissed by user)"), handleError)
+        .then( (choice)=> putStrLn(choice ? choice : "(dismissed by user)"), onReject )
 }
 
 function demoTerm () {
@@ -80,22 +87,22 @@ function demoTerm () {
 }
 
 function demoTextGen (uri :vscode.Uri, token :vscode.CancellationToken) {
-    return vscode.extensions.all.map( (ext) => ext.id ).join('\n')
+    return vscode.extensions.all.map( (ext)=> ext.id ).join('\n')
 }
 
 function printAllCmds () {
-    vscmd.getCommands(true).then(putStrLns , handleError)
+    vscmd.getCommands(true).then(putStrLns , onReject)
 }
 
 function printAllExts () {
     const   path = vscode.Uri.parse('expo://printAllExts'),
-            show = (doc) =>
-                vswin.showTextDocument(doc).then(null, handleError)
-    vsprj.openTextDocument(path).then(show , handleError)
+            show = (doc)=>
+                vswin.showTextDocument(doc).then(null, onReject)
+    vsprj.openTextDocument(path).then(show , onReject)
 }
 
 function printAllLangs () {
-    vscode.languages.getLanguages().then(putStrLns , handleError)
+    vscode.languages.getLanguages().then(putStrLns , onReject)
 }
 
 function putStrLn (ln) {
@@ -123,7 +130,7 @@ function onDocEvent (doc :vscode.TextDocument, eventdesc :string) {
 
 function onDocChanged (evt :vscode.TextDocumentChangeEvent) {
     const   doc = evt.document,
-            msg = () => "EXPO: " + doc.languageId + " file (" + doc.lineCount + " lines) " + doc.uri + ": " + evt.contentChanges.length + " change(s)"
+            msg = ()=> "EXPO: " + doc.languageId + " file (" + doc.lineCount + " lines) " + doc.uri + ": " + evt.contentChanges.length + " change(s)"
     if (isDocWorthy(doc))
         addDisp(vswin.setStatusBarMessage(msg()))
 }
