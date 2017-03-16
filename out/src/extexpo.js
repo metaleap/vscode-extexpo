8b23aa51abee084c13b61b6c9251cc3305579c0f
metaleap <metaleap@mailbox.org>
2017-03-16 04:10:54 +0100

    [autocommit] «Radioactive cats have 18 half-lives.»

--- out/src/extexpo.js
+++ out/src/extexpo.js
@@ -4,54 +4,23 @@ const vscode = require("vscode");
 var vscmd = vscode.commands;
 var vswin = vscode.window;
 var vsprj = vscode.workspace;
-const vsOut = vswin.createOutputChannel("EXPO");
-const disps = [];
-const handleError = (err) => vswin.showErrorMessage("Epic fail: " + err);
-const demoMsgInfo = () => vswin.showInformationMessage("demoMsgInfo ➜ this is `vscode.window.showInformationMessage` in action");
-const demoMsgErr = () => vswin.showErrorMessage("demoMsgErr ➜ this is `vscode.window.showErrorMessage` in action");
-const demoMsgWarn = () => vswin.showWarningMessage("demoMsgWarn ➜ this is `vscode.window.showWarningMessage` in action", "Do dis", "Do dat")
-    .then((choice) => vsOut.appendLine(choice ? choice : "(dismissed by user)"), handleError);
-const demoTextGen = (uri, token) => vscode.extensions.all.map((ext) => ext.id).join('\n');
-const printAllCmds = () => vscmd.getCommands(true).then(printLines, handleError);
-function printAllExts() {
-    const path = vscode.Uri.parse("expo://printAllExts"), show = (doc) => vswin.showTextDocument(doc).then(null, handleError);
-    vsprj.openTextDocument(path).then(show, handleError);
+let vsOut;
+let nterms = 0;
+const lot = [];
+function deactivate() {
+    console.log("EXPO: cleanup");
+    for (const disp of lot)
+        disp.dispose();
+    vsOut.dispose();
 }
-const printAllLangs = () => vscode.languages.getLanguages().then(printLines, handleError);
-function printLines(lines) {
-    vsOut.clear();
-    vsOut.show(false);
-    lines.map((line) => vsOut.appendLine(line));
-}
-function addDisp(disp) {
-    disps.push(disp);
-    return disp;
-}
-function onDocEvent(doc, eventdesc) {
-    if (doc.uri.scheme !== "git")
-        addDisp(vswin.setStatusBarMessage("EXPO: " + doc.languageId + " file (" + doc.lineCount + " lines) " + eventdesc + ": " + doc.uri));
-}
-function onDocOpened(doc) {
-    onDocEvent(doc, "opened from");
-}
-function onDocSaved(doc) {
-    onDocEvent(doc, "saved to");
-}
-function onDocClosed(doc) {
-    onDocEvent(doc, "closed");
-}
-function onDocChanged(evt) {
-    const doc = evt.document, msg = "EXPO: " + doc.languageId + " file (" + doc.lineCount + " lines) " + doc.uri + ": " + evt.contentChanges.length + " change(s)";
-    addDisp(vswin.setStatusBarMessage(msg));
-}
-function onCfgChanged() {
-    addDisp(vswin.setStatusBarMessage("EXPO: configuration changed"));
-}
+exports.deactivate = deactivate;
 function activate(context) {
+    vsOut = vswin.createOutputChannel('EXPO');
     console.log("EXPO: activated");
     const democommands = { 'expo.demoMsgInfo': demoMsgInfo,
         'expo.demoMsgErr': demoMsgErr,
         'expo.demoMsgWarn': demoMsgWarn,
+        'expo.demoTerm': demoTerm,
         'expo.printAllCmds': printAllCmds,
         'expo.printAllExts': printAllExts,
         'expo.printAllLangs': printAllLangs
@@ -60,18 +29,81 @@ function activate(context) {
         context.subscriptions.push(vscmd.registerCommand(cmdname, democommands[cmdname]));
     vsprj.onDidChangeConfiguration(onCfgChanged);
     vsprj.onDidChangeTextDocument(onDocChanged);
-    vsprj.onDidCloseTextDocument(onDocClosed);
-    vsprj.onDidOpenTextDocument(onDocOpened);
-    vsprj.onDidSaveTextDocument(onDocSaved);
+    vsprj.onDidCloseTextDocument((doc) => onDocEvent(doc, "closed"));
+    vsprj.onDidOpenTextDocument((doc) => onDocEvent(doc, "opened from"));
+    vsprj.onDidSaveTextDocument((doc) => onDocEvent(doc, "saved to"));
+    vswin.onDidChangeActiveTextEditor((_) => putStrLn("window.onDidChangeActiveTextEditor"));
+    vswin.onDidChangeTextEditorOptions((_) => putStrLn("window.onDidChangeTextEditorOptions"));
+    vswin.onDidChangeTextEditorSelection((sel) => { if (sel && sel.kind)
+        putStrLn("window.onDidChangeTextEditorSelection"); });
+    vswin.onDidChangeTextEditorViewColumn((_) => putStrLn("window.onDidChangeTextEditorViewColumn"));
+    vswin.onDidChangeVisibleTextEditors((_) => putStrLn("window.onDidChangeVisibleTextEditors"));
+    vswin.onDidCloseTerminal((_) => putStrLn("window.onDidCloseTerminal"));
     const txtgen = { provideTextDocumentContent: demoTextGen };
-    addDisp(vsprj.registerTextDocumentContentProvider("expo", txtgen));
+    addDisp(vsprj.registerTextDocumentContentProvider('expo', txtgen));
 }
 exports.activate = activate;
-function deactivate() {
-    console.log("EXPO: cleanup");
-    for (const disp of disps)
-        disp.dispose();
-    vsOut.dispose();
+function handleError(err) {
+    vswin.showErrorMessage("Error/cancellation/issue: " + err);
 }
-exports.deactivate = deactivate;
+function demoMsgInfo() {
+    vswin.showInformationMessage("demoMsgInfo ➜ this is `vscode.window.showInformationMessage` in action")
+        .then((_) => vswin.showInputBox().then(putStrLn));
+}
+function demoMsgErr() {
+    vswin.showErrorMessage("demoMsgErr ➜ this is `vscode.window.showErrorMessage` in action", "QuickPick palette..")
+        .then((pal) => { if (pal)
+        vswin.showQuickPick(["A choice here..", "another there..", "pick your choice son!"]).then((pick) => putStrLn(pick)); }, handleError);
+}
+function demoMsgWarn() {
+    vswin.showWarningMessage("demoMsgWarn ➜ this is `vscode.window.showWarningMessage` in action", "Do dis", "Do dat")
+        .then((choice) => putStrLn(choice ? choice : "(dismissed by user)"), handleError);
+}
+function demoTerm() {
+    nterms++;
+    const term = vswin.createTerminal("EXPO Term " + nterms);
+    addDisp(term);
+    term.show(false);
+    term.sendText("echo \"You called EXPO on line " + nterms + "?\"");
+}
+function demoTextGen(uri, token) {
+    return vscode.extensions.all.map((ext) => ext.id).join('\n');
+}
+function printAllCmds() {
+    vscmd.getCommands(true).then(putStrLns, handleError);
+}
+function printAllExts() {
+    const path = vscode.Uri.parse('expo://printAllExts'), show = (doc) => vswin.showTextDocument(doc).then(null, handleError);
+    vsprj.openTextDocument(path).then(show, handleError);
+}
+function printAllLangs() {
+    vscode.languages.getLanguages().then(putStrLns, handleError);
+}
+function putStrLn(ln) {
+    vsOut.appendLine(ln);
+}
+function putStrLns(lines) {
+    vsOut.clear();
+    vsOut.show(false);
+    lines.map(putStrLn);
+}
+function addDisp(disp) {
+    lot.push(disp);
+    return disp;
+}
+function isDocWorthy(doc) {
+    return doc.uri.scheme !== "git" && doc.uri.scheme !== "output";
+}
+function onDocEvent(doc, eventdesc) {
+    if (isDocWorthy(doc))
+        addDisp(vswin.setStatusBarMessage("EXPO: " + doc.languageId + " file (" + doc.lineCount + " lines) " + eventdesc + ": " + doc.uri));
+}
+function onDocChanged(evt) {
+    const doc = evt.document, msg = () => "EXPO: " + doc.languageId + " file (" + doc.lineCount + " lines) " + doc.uri + ": " + evt.contentChanges.length + " change(s)";
+    if (isDocWorthy(doc))
+        addDisp(vswin.setStatusBarMessage(msg()));
+}
+function onCfgChanged() {
+    addDisp(vswin.setStatusBarMessage("EXPO: configuration changed"));
+}
 //# sourceMappingURL=extexpo.js.map
--- src/extexpo.ts
+++ src/extexpo.ts
@@ -6,82 +6,26 @@ import vswin = vscode.window
 import vsprj = vscode.workspace
 
 
-const vsOut :vscode.OutputChannel = vswin.createOutputChannel("EXPO")
-const disps :vscode.Disposable[]  = []
+let vsOut   :vscode.OutputChannel
+let nterms  :number                 = 0
+const lot   :vscode.Disposable[]    = []
 
-const handleError = (err :any) =>
-    vswin.showErrorMessage("Epic fail: "+err)
 
-const demoMsgInfo = () =>
-    vswin.showInformationMessage("demoMsgInfo ➜ this is `vscode.window.showInformationMessage` in action")
 
-const demoMsgErr = () =>
-    vswin.showErrorMessage("demoMsgErr ➜ this is `vscode.window.showErrorMessage` in action")
-
-const demoMsgWarn = () =>
-    vswin.showWarningMessage("demoMsgWarn ➜ this is `vscode.window.showWarningMessage` in action", "Do dis", "Do dat")
-            .then((choice) => vsOut.appendLine(choice ? choice : "(dismissed by user)"), handleError)
-
-const demoTextGen = (uri :vscode.Uri, token :vscode.CancellationToken) =>
-    vscode.extensions.all.map( (ext) => ext.id ).join('\n')
-
-const printAllCmds = () =>
-    vscmd.getCommands(true).then(printLines , handleError)
-
-function printAllExts () {
-    const   path = vscode.Uri.parse("expo://printAllExts"),
-            show = (doc) =>
-                vswin.showTextDocument(doc).then(null, handleError)
-    vsprj.openTextDocument(path).then(show , handleError)
+export function deactivate() {
+    console.log("EXPO: cleanup")
+    for (const disp of lot) disp.dispose()
+    vsOut.dispose()
 }
-
-const printAllLangs = () =>
-    vscode.languages.getLanguages().then(printLines , handleError)
-
-function printLines (lines :string[]) {
-    vsOut.clear() ; vsOut.show(false)
-    lines.map( (line) => vsOut.appendLine(line) )
-}
-
-function addDisp (disp :vscode.Disposable) {
-    disps.push(disp)
-    return disp
-}
-
-function onDocEvent (doc :vscode.TextDocument, eventdesc :string) {
-    if (doc.uri.scheme!=="git")
-        addDisp(vswin.setStatusBarMessage("EXPO: " + doc.languageId + " file (" + doc.lineCount + " lines) " + eventdesc + ": " + doc.uri))
-}
-
-function onDocOpened (doc :vscode.TextDocument) {
-    onDocEvent(doc, "opened from")
-}
-
-function onDocSaved (doc :vscode.TextDocument) {
-    onDocEvent(doc, "saved to")
-}
-
-function onDocClosed (doc :vscode.TextDocument) {
-    onDocEvent(doc, "closed")
-}
-
-function onDocChanged (evt :vscode.TextDocumentChangeEvent) {
-    const   doc = evt.document,
-            msg = "EXPO: " + doc.languageId + " file (" + doc.lineCount + " lines) " + doc.uri + ": " + evt.contentChanges.length + " change(s)"
-    addDisp(vswin.setStatusBarMessage(msg))
-}
-
-function onCfgChanged () {
-    addDisp(vswin.setStatusBarMessage("EXPO: configuration changed"))
-}
-
 
 export function activate (context :vscode.ExtensionContext) {
+    vsOut = vswin.createOutputChannel('EXPO')
     console.log("EXPO: activated")
 
     const democommands =    {   'expo.demoMsgInfo': demoMsgInfo
                             ,   'expo.demoMsgErr': demoMsgErr
                             ,   'expo.demoMsgWarn': demoMsgWarn
+                            ,   'expo.demoTerm': demoTerm
                             ,   'expo.printAllCmds': printAllCmds
                             ,   'expo.printAllExts': printAllExts
                             ,   'expo.printAllLangs': printAllLangs
@@ -92,16 +36,98 @@ export function activate (context :vscode.ExtensionContext) {
 
     vsprj.onDidChangeConfiguration(onCfgChanged)
     vsprj.onDidChangeTextDocument(onDocChanged)
-    vsprj.onDidCloseTextDocument(onDocClosed)
-    vsprj.onDidOpenTextDocument(onDocOpened)
-    vsprj.onDidSaveTextDocument(onDocSaved)
+    vsprj.onDidCloseTextDocument( (doc) => onDocEvent(doc, "closed") )
+    vsprj.onDidOpenTextDocument( (doc) => onDocEvent(doc, "opened from") )
+    vsprj.onDidSaveTextDocument( (doc) => onDocEvent(doc, "saved to") )
+    vswin.onDidChangeActiveTextEditor((_) => putStrLn("window.onDidChangeActiveTextEditor"))
+    vswin.onDidChangeTextEditorOptions((_) => putStrLn("window.onDidChangeTextEditorOptions"))
+    vswin.onDidChangeTextEditorSelection((sel) => { if (sel && sel.kind) putStrLn("window.onDidChangeTextEditorSelection") })
+    vswin.onDidChangeTextEditorViewColumn((_) => putStrLn("window.onDidChangeTextEditorViewColumn"))
+    vswin.onDidChangeVisibleTextEditors((_) => putStrLn("window.onDidChangeVisibleTextEditors"))
+    vswin.onDidCloseTerminal((_) => putStrLn("window.onDidCloseTerminal"))
 
     const txtgen = { provideTextDocumentContent : demoTextGen }
-    addDisp(vsprj.registerTextDocumentContentProvider("expo", txtgen))
+    addDisp(vsprj.registerTextDocumentContentProvider('expo', txtgen))
 }
 
-export function deactivate() {
-    console.log("EXPO: cleanup")
-    for (const disp of disps) disp.dispose()
-    vsOut.dispose()
+
+
+function handleError (err :any) {
+    vswin.showErrorMessage("Error/cancellation/issue: "+err)
+}
+
+function demoMsgInfo () {
+    vswin.showInformationMessage("demoMsgInfo ➜ this is `vscode.window.showInformationMessage` in action")
+        .then((_) => vswin.showInputBox().then(putStrLn))
+}
+
+function demoMsgErr () {
+    vswin.showErrorMessage("demoMsgErr ➜ this is `vscode.window.showErrorMessage` in action", "QuickPick palette..")
+        .then( (pal) => { if (pal) vswin.showQuickPick(["A choice here..","another there..","pick your choice son!"]).then((pick) => putStrLn(pick)) } , handleError )
+}
+
+function demoMsgWarn () {
+    vswin.showWarningMessage("demoMsgWarn ➜ this is `vscode.window.showWarningMessage` in action", "Do dis", "Do dat")
+        .then((choice) => putStrLn(choice ? choice : "(dismissed by user)"), handleError)
+}
+
+function demoTerm () {
+    nterms++
+    const term = vswin.createTerminal("EXPO Term " + nterms)
+    addDisp(term)
+    term.show(false)
+    term.sendText("echo \"You called EXPO on line " + nterms + "?\"")
+}
+
+function demoTextGen (uri :vscode.Uri, token :vscode.CancellationToken) {
+    return vscode.extensions.all.map( (ext) => ext.id ).join('\n')
+}
+
+function printAllCmds () {
+    vscmd.getCommands(true).then(putStrLns , handleError)
+}
+
+function printAllExts () {
+    const   path = vscode.Uri.parse('expo://printAllExts'),
+            show = (doc) =>
+                vswin.showTextDocument(doc).then(null, handleError)
+    vsprj.openTextDocument(path).then(show , handleError)
+}
+
+function printAllLangs () {
+    vscode.languages.getLanguages().then(putStrLns , handleError)
+}
+
+function putStrLn (ln) {
+    vsOut.appendLine(ln)
+}
+
+function putStrLns (lines :string[]) {
+    vsOut.clear() ; vsOut.show(false)
+    lines.map(putStrLn)
+}
+
+function addDisp (disp :vscode.Disposable) {
+    lot.push(disp)
+    return disp
+}
+
+function isDocWorthy (doc :vscode.TextDocument) {
+    return doc.uri.scheme!=="git" && doc.uri.scheme!=="output"
+}
+
+function onDocEvent (doc :vscode.TextDocument, eventdesc :string) {
+    if (isDocWorthy(doc))
+        addDisp(vswin.setStatusBarMessage("EXPO: " + doc.languageId + " file (" + doc.lineCount + " lines) " + eventdesc + ": " + doc.uri))
+}
+
+function onDocChanged (evt :vscode.TextDocumentChangeEvent) {
+    const   doc = evt.document,
+            msg = () => "EXPO: " + doc.languageId + " file (" + doc.lineCount + " lines) " + doc.uri + ": " + evt.contentChanges.length + " change(s)"
+    if (isDocWorthy(doc))
+        addDisp(vswin.setStatusBarMessage(msg()))
+}
+
+function onCfgChanged () {
+    addDisp(vswin.setStatusBarMessage("EXPO: configuration changed"))
 }
